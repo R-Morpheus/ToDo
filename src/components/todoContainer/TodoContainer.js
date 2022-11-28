96314f223622a6ddbc9b732594c770d6b5dfa7c0
R-Morpheus <gagach16rus@gmail.com>
2022-11-28 12:16:57 +0300

    add drag and drop list

--- src/components/todoContainer/TodoContainer.js
+++ src/components/todoContainer/TodoContainer.js
@@ -5,7 +5,7 @@ import TodoList from "../todoList/TodoList";
 import StoreApi from "../store/storeApi";
 import InputContainer from "../inputContainer/InputContainer";
 import './todoContainer.css'
-import {DragDropContext} from "react-beautiful-dnd";
+import {DragDropContext, Droppable} from "react-beautiful-dnd";
 
 const TodoContainer = () => {
     const [data, setData] = useState(store)
@@ -60,11 +60,18 @@ const TodoContainer = () => {
     }
 
     const onDragEnd = (result) => {
-        const {destination, source, draggableId} = result
+        const {destination, source, draggableId, type} = result
         console.log('destination', destination, 'source', source, draggableId);
 
         if (!destination){
             return
+        }
+
+        if (type === 'list'){
+            const newListIds = data.listIds
+            newListIds.splice(source.index, 1)
+            newListIds.splice(destination.index, 0, draggableId)
+            return;
         }
         const sourceList = data.lists[source.droppableId];
         const destinationList = data.lists[destination.droppableId];
@@ -83,66 +90,40 @@ const TodoContainer = () => {
                 },
             }
             setData(newState)
-        }
-        // const { destination, source, draggableId, type } = result;
-        // console.log('destination', destination, 'source', source, draggableId);
-        //
-        // if (!destination) {
-        //     return;
-        // }
-        // if (type === 'list') {
-        //     const newListIds = data.listIds;
-        //     newListIds.splice(source.index, 1);
-        //     newListIds.splice(destination.index, 0, draggableId);
-        //     return;
-        // }
+        } else {
+            sourceList.cards.splice(source.index, 1)
+            destinationList.cards.splice(destination.index, 0, draggingCard)
 
-    //     const sourceList = data.lists[source.droppableId];
-    //     const destinationList = data.lists[destination.droppableId];
-    //     const draggingCard = sourceList.cards.filter(
-    //         (card) => card.id === draggableId
-    //     )[0];
-    //
-    //     if (source.droppableId === destination.droppableId) {
-    //         sourceList.cards.splice(source.index, 1);
-    //         destinationList.cards.splice(destination.index, 0, draggingCard);
-    //         const newSate = {
-    //             ...data,
-    //             lists: {
-    //                 ...data.lists,
-    //                 [sourceList.id]: destinationList,
-    //             },
-    //         };
-    //         setData(newSate);
-    //     } else {
-    //         sourceList.cards.splice(source.index, 1);
-    //         destinationList.cards.splice(destination.index, 0, draggingCard);
-    //
-    //         const newState = {
-    //             ...data,
-    //             lists: {
-    //                 ...data.lists,
-    //                 [sourceList.id]: sourceList,
-    //                 [destinationList.id]: destinationList,
-    //             },
-    //         };
-    //         setData(newState);
-    //     }
-    // };
+            const newState = {
+                ...data,
+                lists: {
+                    ...data.lists,
+                    [sourceList.id]: sourceList,
+                    [destinationList.id]: destinationList,
+                },
+            }
+            setData(newState)
+        }
+
     };
 
     return (
-        <div className='todoContainer'>
-            <StoreApi.Provider value={{addCard, addList, updateListTitle}}>
-                <DragDropContext onDragEnd={onDragEnd}>
-                    {data.listIds.map((listId) => {
-                        const list = data.lists[listId]
-                        return <TodoList list={list} key={listId}/>
-                    })}
-                    <InputContainer type='list'/>
-                </DragDropContext>
-            </StoreApi.Provider>
-        </div>
+        <StoreApi.Provider value={{addCard, addList, updateListTitle}}>
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable droppableId='todoContainer' type='list' direction='horizontal'>
+                    {(provided) => (
+                        <div className='todoContainer' ref={provided.innerRef} {...provided.droppableProps}>
+                            {data.listIds.map((listId, index) => {
+                                const list = data.lists[listId]
+                                return <TodoList list={list} key={listId} index={index}/>
+                            })}
+                            <InputContainer type='list'/>
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
+        </StoreApi.Provider>
     );
 };
 
--- src/components/todoList/TodoList.js
+++ src/components/todoList/TodoList.js
@@ -3,24 +3,30 @@ import TodoTitle from "./TodoTitle";
 import './todoList.css'
 import TodoCard from "../todoCard/TodoCard";
 import InputContainer from "../inputContainer/InputContainer";
-import {Droppable} from "react-beautiful-dnd";
+import {Draggable, Droppable} from "react-beautiful-dnd";
 
-const TodoList = ({list}) => {
+const TodoList = ({list, index}) => {
     return (
-        <div className='list__container'>
-            <TodoTitle title={list.title} listId={list.id}/>
-            <Droppable droppableId={list.id}>
-                {(provided) => (
-                    <div ref={provided.innerRef} {...provided.droppableProps} className='card__container'>
-                        {list.cards.map((card, index) => (
-                            <TodoCard key={card.id} card={card} index={index}/>
-                        ))}
-                        {provided.placeholder}
+        <Draggable draggableId={list.id} index={index}>
+            {(provided) => (
+                <div className='list__container' ref={provided.innerRef} {...provided.draggableProps}>
+                    <div {...provided.dragHandleProps}>
+                        <TodoTitle title={list.title} listId={list.id}/>
+                        <Droppable droppableId={list.id}>
+                            {(provided) => (
+                                <div ref={provided.innerRef} {...provided.droppableProps} className='card__container'>
+                                    {list.cards.map((card, index) => (
+                                        <TodoCard key={card.id} card={card} index={index}/>
+                                    ))}
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
+                        <InputContainer listId={list.id} type='card'/>
                     </div>
-                )}
-            </Droppable>
-            <InputContainer listId={list.id} type='card'/>
-        </div>
+                </div>
+            )}
+        </Draggable>
     );
 };
 
